e5e11d4ba9b165dac2d18bad25527d8fa3f95047
Ardang666 <ardang2015@gmail.com>
2022-10-19 10:42:19 +0000

    fix build

--- mm/mm_gen_model/build_magicmind_model.sh
+++ mm/mm_gen_model/build_magicmind_model.sh
@@ -9,16 +9,16 @@ CMD='/usr/local/neuware/samples/magicmind/mm_build/build/onnx_build'
 # float32的模型生成过程
 QUANT_MODE=force_float32
 echo ${QUANT_MODE}
-#${CMD} --onnx ${ONNX_MODEL} --precision ${QUANT_MODE} --build_config config.json --magicmind_model ${MM_MODEL_PATH}/${PROJ_NAME}.${QUANT_MODE}.magicmind
+${CMD} --onnx ${ONNX_MODEL} --precision ${QUANT_MODE} --build_config config.json --magicmind_model ${MM_MODEL_PATH}/${PROJ_NAME}.${QUANT_MODE}.magicmind
 
 # float16的模型生成过程
 QUANT_MODE=force_float16
 echo ${QUANT_MODE}
-#${CMD} --onnx ${ONNX_MODEL} --precision ${QUANT_MODE} --build_config config.json --magicmind_model ${MM_MODEL_PATH}/${PROJ_NAME}.${QUANT_MODE}.magicmind
+${CMD} --onnx ${ONNX_MODEL} --precision ${QUANT_MODE} --build_config config.json --magicmind_model ${MM_MODEL_PATH}/${PROJ_NAME}.${QUANT_MODE}.magicmind
 
 # int8 量化模型的生成过程，这里使用公开数据集；根据需要指向自己的数据集
 DATASETS_PATH=/public/datasets/COCO/val2017
-DATASETS_PATH=/nfsdata/datasets/COCO2017/images/train2017/
+# DATASETS_PATH=/nfsdata/datasets/COCO2017/images/train2017/
 QUANT_MODE=qint8_mixed_float16
 echo ${QUANT_MODE}
 python gen.py --onnx_model ${ONNX_MODEL} --shape_mutable true --quant_mode ${QUANT_MODE} --output ${MM_MODEL_PATH}/${PROJ_NAME}.${QUANT_MODE}.magicmind --image_dir ${DATASETS_PATH} 
